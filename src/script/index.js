32a5c23775e52445a421b58a806861d013d0fa01
Zi Ye <18612319597@163.com>
2020-01-25 17:56:36 +0800

    [feature] build js and react with webpack

--- src/script/index.js
+++ src/script/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import { fn2 } from "./util";
 
 class Demo extends Component {
     render() {
+        fn2();
         return <div className="box">{this.props.text}</div>
     }
 }
---
+++ src/script/util.js
@@ -0,0 +1,7 @@
+export const fn1 = () => {
+    console.log('This is fn1');
+};
+
+export const fn2 = () => {
+    console.log('This is fn2');
+};
